0889814440d8e5800f67515210462a08b3303317
valeriansword <chandrakumarxii@gmail.com>
2024-12-05 13:47:49 +0530

    added url and port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app=express();
 const userRouter=require("./routes/userRoute.js");
 app.use(express.json())
 app.use(cors({
-    origin:["https://e-comclient.onrender.com","http://localhost:5173"],
+    origin:["*"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], // Add other HTTP methods if needed
     allowedHeaders: ["Content-Type", "Authorization"] 
