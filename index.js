327bec2d028340a70e9d2f15696725d4bfb42983
valeriansword <chandrakumarxii@gmail.com>
2024-12-05 14:12:24 +0530

    images

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app=express();
 const userRouter=require("./routes/userRoute.js");
 app.use(express.json())
 app.use(cors({
-    origin:["*"],
+    origin:["https://e-comclient.onrender.com","http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], // Add other HTTP methods if needed
     allowedHeaders: ["Content-Type", "Authorization"] 
