f723eeeae7c69bb7026f2b03b0716deaaf33bc2d
valeriansword <chandrakumarxii@gmail.com>
2024-12-05 13:28:36 +0530

    added url

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const app=express();
 const userRouter=require("./routes/userRoute.js");
 app.use(express.json())
 app.use(cors({
-    origin:["https://e-comclient.onrender.com","http://localhost:5173"]
+    origin:["https://e-comclient.onrender.com","http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Add other HTTP methods if needed
+    allowedHeaders: ["Content-Type", "Authorization"] 
 }));
 dotenv.config()
 app.use("/images", express.static(path.join(__dirname, "upload", "images")));
